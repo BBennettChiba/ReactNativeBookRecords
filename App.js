b3ed47570f6590f1ad35892406a9f34c322688c6
BBennettChiba <BrysonCBennett@gmail.com>
2021-05-22 17:05:43 +0900

    added authentication

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState, useContext } from "react";
-import { View, StyleSheet, Button, FlatList } from "react-native";
-import Amplify from "aws-amplify";
+import React from "react";
+import Amplify, { Auth } from "aws-amplify";
 import config from "./src/aws-exports";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./navigation/Tabs";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { UserContextProvider } from "./contexts/UserContext";
+import { withAuthenticator } from "aws-amplify-react-native";
 
-Amplify.configure(config);
+Amplify.configure({ ...config, Analytics: { disabled: true } });
 
 const App = () => {
   return (
@@ -21,20 +21,4 @@ const App = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-  },
-  book: { marginBottom: 15 },
-  input: { height: 50, backgroundColor: "#ddd", marginBottom: 10, padding: 8 },
-  bookName: { fontSize: 18 },
-  button: {
-    flex: 1,
-  },
-  bookBox: {
-    flexDirection: "row",
-  },
-});
-
-export default App;
+export default withAuthenticator(App);
--- contexts/UserContext.js
+++ contexts/UserContext.js
@@ -1,6 +1,7 @@
 import React, { createContext, useState, useContext, useEffect } from "react";
 import { getUser } from "../src/graphql/queries";
-import { API, graphqlOperation } from "aws-amplify";
+import { API, graphqlOperation, Auth } from "aws-amplify";
+import { createUser } from "../src/graphql/mutations";
 
 const UserContext = createContext(null);
 const UserContextUpdate = createContext(null);
@@ -9,11 +10,10 @@ export function useUser() {
   return useContext(UserContext);
 }
 export function useUserUpdate() {
-    return useContext(UserContextUpdate);
+  return useContext(UserContextUpdate);
 }
 
 export function UserContextProvider({ children }) {
-  const me = { name: "Bryson", id: "c83d9cb1-aaf2-4fcd-8dd5-a38aab6ce485" };
   const [user, setUser] = useState({});
 
   useEffect(() => {
@@ -22,17 +22,35 @@ export function UserContextProvider({ children }) {
 
   async function fetchBooks() {
     try {
-      let userData = await API.graphql(graphqlOperation(getUser, {id:me.id}));
-      userData = userData.data.getUser;
-      setUser(userData)
+      let id;
+      let username;
+      id = await Auth.currentUserInfo();
+      username = id.username;
+      id = id.id;
+      let userData;
+      userData = (await API.graphql(graphqlOperation(getUser, { id }))).data.getUser
+      if (userData === null) {
+        userData = createNewUser(id, username);
+      }
+      setUser(userData);
     } catch (err) {
       console.log("error fetching books", err);
     }
   }
 
+  async function createNewUser(id, username) {
+    console.log("working?");
+    let user = (
+      await API.graphql(graphqlOperation(createUser, {
+        input: { id, name:username },
+      }))
+    ).data.createUser;
+    return user;
+  }
+
   return (
     <UserContext.Provider value={user}>
-      <UserContextUpdate.Provider value={setUser} >
+      <UserContextUpdate.Provider value={setUser}>
         {children}
       </UserContextUpdate.Provider>
     </UserContext.Provider>
